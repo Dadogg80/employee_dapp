dfba1c4fa3adaf8ad2dac37ea00ecbfa00d1743d
dadogg80 <ivo.garofalo@gmail.com>
2020-12-20 18:36:35 +0100

    Unit Testing

--- test/test.js
+++ test/test.js
@@ -1,17 +1,26 @@
+import truffleAssert from 'truffle-assertions';
 const Employees = artifacts.require("Employees");
 
+const department = "0x1bE21d22f40de95150c80f0761C98d99a390298C";
+const worker1 = "0x34f213A881b4D01cfBfEc9e8D55185E3A3fF92E3";
+const worker2 = "0x17BCd7378a9d6025ffACd9C91286a71B6ae08209";
+
 contract("Employees", async (accounts) => {
-    it("should create a new employee", async () => {
+    it("should create a first new employee", async () => {
         let instance = await Employees.deployed();
-        let employee = await instance.createEmployee("Ivo", "NORWAY", 10102020, "email@email.com", "0x34f213A881b4D01cfBfEc9e8D55185E3A3fF92E3", "0x1bE21d22f40de95150c80f0761C98d99a390298C", 10);
-        let result = await instance.getEmployee(0);
+        let employee = await instance.createEmployee("Ivo", "NORWAY", 10102020, "email@email.com", worker1, department, 10);
         assert(employee == 0);
     });
-    it("should create a new employee", async () => {
+    it("should create a second new employee", async () => {
         let instance = await Employees.deployed();
-        let employee = await instance.createEmployee("Sienna", "NORWAY", 10102020, "test@email.com", "0x17BCd7378a9d6025ffACd9C91286a71B6ae08209", "0x1bE21d22f40de95150c80f0761C98d99a390298C", 20);
-        let result = await instance.getEmployee(1);
+        let employee = await instance.createEmployee("Sienna", "NORWAY", 10102020, "test@email.com", worker2, department, 20);
         assert(employee == 1);
     });
 
 });
+
+
+/* 
+await truffleAssert.fails(instance.createWorker("Ivo", "NORWAY", 10102020, "email@email.com", worker1, department, 10, {}));
+
+*/
