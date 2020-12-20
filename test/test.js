7fc573714599aed6533e8c2bf76dac23738edfe2
dadogg80 <ivo.garofalo@gmail.com>
2020-12-20 23:06:20 +0100

    Unit Tests

--- test/test.js
+++ test/test.js
@@ -15,13 +15,13 @@ contract("Employees", async () => {
     it("should create a second new employee", async () => {
         let instance = await Employees.deployed();
         await instance.createEmployee("Sienna", "Norway", "10102020", "Sienna@email.com", worker2, department, 20);
+        
         let employee = await instance.getEmployee(1);
         assert(employee.id == 1);
     });
-    it("shouldn't create a third new employee", async () => {
+    it("shouldn't let the worker address and department address be the same address", async () => {
         let instance = await Employees.deployed();
-        await truffleAssert.fails(instance.createEmployee("Samuel", "Norway", "21.12.2020", "Samuel@email.com", department, department, 20));
-        
+        await truffleAssert.fails(instance.createEmployee("Samuel", "Norway", "21.12.2020", "Samuel@email.com", department, department, 20), truffleAssert.ErrorType.REVERT);
     });
 });
 
